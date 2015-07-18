905f9168299225b5a2df6cd252b1a8ce1a62a9f3
Sammy Pfeiffer <sammypfeiffer@gmail.com>
2015-07-18 16:46:36 +0200

    Initial commit

---
+++ diary.md
@@ -0,0 +1,61 @@
+Let's make mosaics from instagram photos!
+
+===
+
+Recently I did some web scrapping finding users in instagram and their profile photo and I thought I could play a bit more with it.
+
+Years ago I also played with making mosaics from photos using python for a project in the university, let's mix both things.
+
+I plan to create a mosaic of the profile photo of an instagram user using their own photos.
+
+===
+
+Our tools:
+
+* Ubuntu 14.04
+* Python 2.7
+* `requests` python library
+* `osaic` [python library for doing mosaics](https://pypi.python.org/pypi/osaic/2.0.0)
+* `python-instagram` [instagram official python API](https://github.com/Instagram/python-instagram)
+
+To install osaic correctly I needed to do:
+
+    # Make sure I have the necessary libraries
+    sudo apt-get install libjpeg-dev libfreetype6-dev zlib1g-dev libpng12-dev
+    # soft link some stuff
+    sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib
+    sudo ln -s /usr/lib/x86_64-linux-gnu/libfreetype.so /usr/lib
+    sudo ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib
+    sudo ln -s /usr/include/freetype2 /usr/include/freetype
+    # install PIL with these extra flags
+    sudo pip install PIL --allow-external PIL --allow-unverified PIL
+    # install osaic
+    sudo pip install osaic
+
+===
+
+First step, let's get the profile photo of a user.
+
+I've done that in `get_ig_profile_photo.py` using the requests library to get the website of an user (http://instagram.com/username) and parsing the website finding the place where the profile photo url is found.
+
+The profile photo is in the tag `<meta property="og:image" content="`. So I search for the next closing quotes to get the url. The url looks like:
+https://igcdn-photos-h-a.akamaihd.net/hphotos-ak-xfa1/t51.2885-19/11428697_944597985591767_1329843489_a.jpg
+
+Once I have the url I download the user photo and save it to disk using the requests library again `requests.get(user_img_url, stream=True)`.
+
+The final result is executing:
+
+    ./get_ig_profile_photo.py r0sw3l
+	Getting profile photo of r0sw3l at: http://instagram.com/r0sw3l
+	User profile image url: https://igcdn-photos-h-a.akamaihd.net/hphotos-ak-xfa1/t51.2885-19/11428697_944597985591767_1329843489_a.jpg
+	Saved r0sw3l.jpg
+
+Which downloads my IG profile photo:
+
+
+
+
+
+
+
+
---
+++ get_ig_profile_photo.py
@@ -0,0 +1,77 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+18/07/15
+@author: Sammy Pfeiffer
+
+get_ig_profile_photo.py downloads
+the profile photo of a given instagram username
+if it exists.
+
+"""
+
+import sys
+import requests
+import shutil
+
+def chars_until_quotes(mystr):
+    for i in range(400):
+        if mystr[i] == '"':
+            return i
+    return None
+
+def get_user_image_url(web_str):
+    """
+    Given the website string return the url of the user image
+    :param web_str: the website in a string format
+    :return: str with the url of the user image
+    """
+    # The user image is found in this tag
+    interesting_prop = '<meta property="og:image" content="'
+    try:
+        initial_index = web_str.index(interesting_prop) + len(interesting_prop)
+    except ValueError:
+        #print "ERROR: substring not found"
+        return None
+    # The url changes of size, so just check in the next 400 characters for the closing quotes
+    image_url_len = chars_until_quotes(web_str[initial_index:initial_index+400])
+    if not image_url_len:
+        #print "ERROR: No url found for user image"
+        return None
+    user_img_url = web_str[initial_index:initial_index + image_url_len]
+    print "User profile image url: " + user_img_url
+    return user_img_url
+
+
+def get_profile_photo_from_username(username, filename=None):
+    ig_url = "http://instagram.com/"
+    print "Getting profile photo of " + username + " at: " + ig_url + username
+    # Get the website
+    r = requests.get(ig_url+username)
+    web_str = r.content
+    # Parse it for the user image url
+    user_img_url = get_user_image_url(web_str)
+
+    if user_img_url:
+        # Download image with username
+        response = requests.get(user_img_url, stream=True)
+        if filename is None:
+            filename = username
+        # Save the image
+        with open(filename + '.jpg', 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+        print "Saved " + filename + ".jpg"
+    else:
+        print "Username not found."
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print "You must provide an instagram username to download their profile photo."
+        print "Usage:"
+        print sys.argv[0] + " username"
+        exit(0)
+    username = sys.argv[1]
+    get_profile_photo_from_username(username)
+
